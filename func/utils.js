01ad4aa986b68aa2171db5a164b397886cd7f94f
pseudopirate <pseudopirate@ya.ru>
2021-08-22 13:20:49 +0300

    hashtag some messages

--- func/utils.js
+++ func/utils.js
@@ -4,7 +4,7 @@ const getChatId = (ctx) => _.get(ctx, ['update', 'message', 'chat', 'id'])
 const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
 
 const prepareBdaysReply = (bdays) => {
-    return bdays.map((item) =>
+    return '#birthdays\n' + bdays.map((item) =>
         `${_.padStart(item.day, 2, '0')} ${months[item.month - 1]} - <b>${item.name}</b>`).join('\n')
 }
 
