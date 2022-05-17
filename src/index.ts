fda7da2de1c30a5172e3661ef23f083d96ba913f
pseudopirate <pseudopirate@ya.ru>
2022-05-17 11:34:12 +0300

    fix: start bot

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-// import { bot } from './bot'; // eslint-disable-line import/first
+import { bot } from './bot'; // eslint-disable-line import/first
 import { runCronJobs } from './cron'; // eslint-disable-line import/first
 
-// bot.launch();
-// console.log('Bot started');
+bot.launch();
+console.log('Bot started');
 
 runCronJobs();
 console.log('Cron jobs started');
